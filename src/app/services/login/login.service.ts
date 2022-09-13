7418f395a70912ad1230dad7be58430ec6e578b3
Miltownee <72917466+Miltownee@users.noreply.github.com>
2022-09-13 10:25:23 +0200

    feat: created alot of things, axios, loginservice, registerform

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,8 +1,10 @@
 import { map, Observable, of, switchMap } from "rxjs";
 import { environment } from "src/environments/environment";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { User } from "src/app/models/user.model";
+import { DateTimePickerComponent } from "@progress/kendo-angular-dateinputs";
+import axios from "axios";
 
 
 // Variable for getting the Key and Url from the environment file
@@ -24,7 +26,7 @@ export class LoginService {
             switchMap((user: User | undefined) => {
                 // If user is undefined a user will be created with the given name
                 if (user === undefined) {
-                    return this.createUser(username);
+                    return this.createUser();
                 }
                 return of(user);
             })
@@ -40,20 +42,44 @@ export class LoginService {
     }
 
     // Checks if the user exists
-    private createUser(username: string): Observable<User> {
-        const user = {
-            username,
-            shipments: []
-        };
+    public createUser(): Observable<User> {
+        const user = { FirstName: "adam",
+        LastName: "Larsson",
+        Email: "pelle@larsson.com",
+        Password: "abcde123",
+        Country: "SV",
+        PostalCode: 12345,
+        PhoneNumber: 701234567,
+        DataOfBirth: Date.now(),
+        AccountType: 1 }
         // Header / API Key
         const headers = new HttpHeaders({
-            "Content-Type": "application/json",
-            "x-api-key": API_KEY
+            "Content-Type": "application/json"
         });
         // POST - Creates user on the server
         return this.http.post<User>(apiUsers, user, {
-            headers
+            // headers
         })
+        // .subscribe({
+        //     error: (error: HttpErrorResponse) => { // this._error can be displayed via the get methods in other components
+        //       this._error = error.message; // ... to read the error 
+        //     }
+        //   })
+    }
+    public createUserAxios(FirstName: string, LastName: string, email: string, password: string, country: string, postalcode: number, phonenumber: number ){
+        const user = { FirstName: FirstName,
+        LastName: LastName,
+        Email: email,
+        Password: password,
+        Country: country,
+        PostalCode: postalcode,
+        PhoneNumber: phonenumber,
+        DataOfBirth: Date.now(),
+        AccountType: 1 
+    }
+
+
+axios.post(apiUsers,user).then((response) => console.log(response))
     }
 
 
